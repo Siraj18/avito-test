4e61c8c4e0704892dd4c3c4359d242845666150f
Siraj <sgsdbs@gmail.com>
2022-02-25 02:10:27 +0300

    Complete postgres pkg

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module github.com/siraj18/avito-test
 go 1.17
 
 require github.com/go-chi/chi/v5 v5.0.7
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -1,5 +1,28 @@
 package postgres
 
-func NewDb(conStr string) error {
-	return nil
+import (
+	"context"
+	"fmt"
+	"time"
+
+	_ "github.com/jackc/pgx"
+	"github.com/jmoiron/sqlx"
+)
+
+func NewDb(conStr string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("pgx", conStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return db, nil
 }
