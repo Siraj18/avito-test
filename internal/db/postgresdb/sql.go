7d780ba5705b3be1a65bbc565bc795f0666a5982
Siraj <sgsdbs@gmail.com>
2022-02-25 22:19:59 +0300

    add basic sql commands

--- internal/db/postgresdb/sql.go
+++ internal/db/postgresdb/sql.go
@@ -1,23 +1,37 @@
 package postgresdb
 
 const initSchema = `
-			DROP TABLE IF EXISTS transactions;
-			DROP TABLE IF EXISTS users;
-			
-			CREATE TABLE IF NOT EXISTS users 
-			(
-				id  	UUID PRIMARY KEY,
-				balance DECIMAL(10, 2) DEFAULT 0
-			);
-			CREATE TABLE IF NOT EXISTS transactions
-			(
-				id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-				to_id      UUID REFERENCES users(id),
-				from_id    UUID REFERENCES users(id),
-				money      DECIMAL(10, 2) NOT NULL,
-				method 	   TEXT NOT NULL,
-				created_at TIMESTAMP DEFAULT now()
-			);
-			CREATE INDEX ON transactions (to_id);
-			CREATE INDEX ON transactions (from_id);
+				DROP TABLE IF EXISTS transactions;
+				DROP TABLE IF EXISTS users;
+				
+				CREATE TABLE IF NOT EXISTS users 
+				(
+					id  	UUID PRIMARY KEY,
+					balance DECIMAL(10, 2) DEFAULT 0
+				);
+				CREATE TABLE IF NOT EXISTS transactions
+				(
+					id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+					to_id      UUID REFERENCES users(id),
+					from_id    UUID REFERENCES users(id),
+					money      DECIMAL(10, 2) NOT NULL,
+					method 	   TEXT NOT NULL,
+					created_at TIMESTAMP DEFAULT now()
+				);
+				CREATE INDEX ON transactions (to_id);
+				CREATE INDEX ON transactions (from_id);
+`
+
+const addUserSql = `
+				INSERT INTO users VALUES ($1, 0);
+`
+
+const updateUserBalanceSql = `
+				UPDATE users SET balance=balance + $2
+				WHERE id=$1;	
+`
+
+const getUserBalanceSql = `
+				SELECT id, balance FROM users
+				WHERE id=$1;
 `
